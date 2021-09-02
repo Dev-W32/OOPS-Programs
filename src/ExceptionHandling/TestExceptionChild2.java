af408e0e9525936d8828464d9114cf6e8a18cc17
Devesh Wadhwani <wadhwanids_1@rknec.edu>
2021-09-02 12:31:15 +0530

    method overriding and exception handling 2

--- src/ExceptionHandling/TestExceptionChild2.java
+++ src/ExceptionHandling/TestExceptionChild2.java
@@ -11,7 +11,8 @@ class Parent2{
 public class TestExceptionChild2 extends Parent2{
     // overriding method in child class
     // There is no compile time error even if child class dosent declare exception
-    void msg() throws ArithmeticException //throws IOException will result in error
+    // But no need of writing throws and exception name in child class
+    void msg() /*no need of this but still writing this lol*/ throws ArithmeticException //throws IOException will result in error
     {
         System.out.println("TestExceptionChild2");
     }
